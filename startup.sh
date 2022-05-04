1ab405884330924f3e84ce46bc9fec069d7e3558
jessiefff <jiefangli0605@gmail.com>
2022-05-04 18:14:44 -0400

    fixed untracked files

--- startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export mongodb_URI='mongodb+srv://jiefangli:gsxpUXJi4FuuPTWW@branda-backend.8qsjk.mongodb.net/DB?retryWrites=true&w=majority
-'
-echo "connecting to $mongodb_URI"
-nodemon
